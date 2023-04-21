9492ab40f84cebfa2a9c4c37c445c677b51c070b
Gabriel Fierro <fierrogabriel13@gmail.com>
2023-04-21 20:38:44 -0300

    add: the id items and footer to manipulate and display his properly data

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 // Variables zone
 const cards = document.getElementById("cards");
+const items = document.getElementById("items");
+const footer = document.getElementById("footer");
 const templateCard = document.getElementById("template-card").content;
+const templateFooter = document.getElementById("template-footer").content;
 const fragment = document.createDocumentFragment();
 let cart = {}; // It's an object without elements
 
